33469b1f7323416aa2c0d2e9680b13fce93d45b0
varshabade <varshaankushandhalle@gmail.com>
2023-08-06 20:43:02 +0530

    Editted

--- test_file003.py
+++ test_file003.py
@@ -1,15 +1,16 @@
 import allure
+#This is Test file 3
 class Test_Credence_003:
 
-    # def test_mul_004(self):
-    #     a = 3
-    #     b = 7
-    #     mul = a * b
-    #     print("Mul of a and b is :" + str(mul))
-    #     if mul == 21:
-    #         assert True
-    #     else:
-    #         assert False
+    def test_mul_004(self):
+        a = 3
+        b = 7
+        mul = a * b
+        print("Mul of a and b is :" + str(mul))
+        if mul == 21:
+            assert True
+        else:
+            assert False
 
     def test_sub_005(self):
         a = 12
@@ -18,5 +19,5 @@ class Test_Credence_003:
         print("Subtraction of a from b is :" + str(sub))
         if sub == 5:
             assert True
-        else:
-            assert False
+    #     else:
+    #         assert False
