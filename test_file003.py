85c895bf329f0ee3b8a3245d66d264687f04f2a2
varshabade <varshaankushandhalle@gmail.com>
2023-08-06 19:42:39 +0530

    new comment

--- test_file003.py
+++ test_file003.py
@@ -1,15 +1,15 @@
 import allure
 class Test_Credence_003:
 
-    def test_mul_004(self):
-        a = 3
-        b = 7
-        mul = a * b
-        print("Mul of a and b is :" + str(mul))
-        if mul == 21:
-            assert True
-        else:
-            assert False
+    # def test_mul_004(self):
+    #     a = 3
+    #     b = 7
+    #     mul = a * b
+    #     print("Mul of a and b is :" + str(mul))
+    #     if mul == 21:
+    #         assert True
+    #     else:
+    #         assert False
 
     def test_sub_005(self):
         a = 12
